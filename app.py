507140e9b6c291b4392d32e20ef01bfdaa43deeb
uptobe8 <ia.uptobe@gmail.com>
2025-05-08 03:26:33 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Servidor funcionando correctamente."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
