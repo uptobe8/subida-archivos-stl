e021c8dc73438ae32f274d7180f7a7db6a5813a1
uptobe8 <ia.uptobe@gmail.com>
2025-08-06 02:45:10 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Servidor funcionando correctamente."
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
